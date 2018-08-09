eca66ee46bc9e32f84a48b79bdc2b1949a400bca
qitianyi <879777342@qq.com>
2018-08-09 11:04:15 +0800

    update

--- static/js/jump.js
+++ static/js/jump.js
@@ -1,8 +1,7 @@
   $(function(){
-  		$('.jump1 input').keyup(
-  				function(){
-  					location.href="children_"+$('.jump1 input').val()+".html";
-  				}
-  			)
-                        
+  		
+        $('.jump2').click(function(event) {
+  			/* Act on the event */
+  			location.href="children_"+$('.jump1 input').val()+".html";
+  		});          
   })
